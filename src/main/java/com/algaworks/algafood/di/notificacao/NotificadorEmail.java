2a7a866db3ae8b1f4d0337933c99148b332498fe
Giancarlo Lucciani <giancarlolucciani@gmail.com>
2022-08-20 20:29:09 -0300

    acessando propriedade customizadas

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -10,8 +11,16 @@ import com.algaworks.algafood.di.modelo.Cliente;
 @Component
 public class NotificadorEmail implements Notificador {
 
+	@Value("${notificador.email.host-servidor}")
+	private String host;
+
+	@Value("${notificador.email.porta-servidor}")
+	private Integer porta;
+
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+		System.err.println("Host: " + host);
+		System.err.println("Porta: " + porta);
 		System.out.printf("Notificando %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 
